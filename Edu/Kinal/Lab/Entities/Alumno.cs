1a1ecd155c8a2f44319e21652b51f60b6e4c5b16
alvaro florez flores perez <alvaritotutos@gmail.com>
2022-03-15 14:58:04 -0600

    Asistencia, Datos y Asignaturas hechas

--- Edu/Kinal/Lab/Entities/Alumno.cs
+++ Edu/Kinal/Lab/Entities/Alumno.cs
@@ -29,36 +29,18 @@ namespace Lab2.Edu.Kinal.Lab.Entities
 
         override public string ToString()
         {
-            return $"El alumno {this.Nombres} {this.Apellidos} con carné {this.Carne} y con créditos {this.NumeroCreditos}";
+            return $"el alumno {this.Nombres} {this.Apellidos} con carné {this.Carne} y con créditos {this.NumeroCreditos}";
         }
 
         public void listarMisDatos(string identificador)
         {
-            string carneEncontrado = identificador;
-            if (this.Carne == carneEncontrado)
-            {
-                Console.WriteLine($"El alumno {this.Nombres} {this.Apellidos} con carné {this.Carne} y con créditos {this.NumeroCreditos}");
-            }
-            else
-            {
-                Console.WriteLine("Este alumno no tiene este carné");
-            }
+            Console.WriteLine($" El alumno con carné {this.Carne}, apellidos, {this.Apellidos}, nombres, {this.Nombres}, y con {this.NumeroCreditos} créditos");
         }
 
         public bool eliminarAsignatura(string asignatura)
         {
-            string viejaAsignatura = asignatura;
-            if (this.Asignatura == asignatura)
-            {
-                this.NumeroCreditos = 0;
-                Console.WriteLine($"El alumno {this.Nombres} ya no tiene el curso  {this.Asignatura}  y tiene {this.NumeroCreditos} créditos");
-                return true;
-            }
-            else
-            {
-                Console.WriteLine($"Esta asignatura no existe");
-                return false;
-            }
+            Console.WriteLine($"Se ha eliminado la asignatura {this.Asignatura} de {this.Apellidos} {this.Nombres}, Perdera la cantidad de creditos de {this.NumeroCreditos}");
+            return true;
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,53 +1,55 @@
 ﻿using Lab2.Edu.Kinal.Lab.Entities;
+using Lab2.Edu.Kinal.Lab.Interfaz;
+
 public class Program
 {
     private static List<Persona> listaPersonas = new List<Persona>();
     public static void Main(string[] args)
     {
         Persona alumno1 = new Alumno("4580K", "Flores", "Alvaro", "alvaro@gmail.com", "2016579", 50, "Idioma");
-        Persona alumno2 = new Alumno("4580K", "Flores", "Alvaro", "alvaro@gmail.com", "2016482", 20, "Ciencias");
-        Persona profesor1 = new Profesor("4580J", "Flores", "Alvaro", "alvaro@gmail.com", "3020151910101", "Profesor de ciencias");
+        Persona alumno2 = new Alumno("4580L", "Guevara", "Carlos", "carlos@gmail.com", "2016482", 20, "Ciencias");
+        Persona profesor1 = new Profesor("4580J", "Pérez", "Leonardo", "leonardo@gmail.com", "3020254800101", "Profesor de ciencias");
 
-        listaPersonas.Add(alumno1);
-        listaPersonas.Add(alumno2);
-        listaPersonas.Add(profesor1);
+        /* Registro */
+        operarRegistro(alumno1);
+        operarRegistro(alumno2);
+        operarRegistro(profesor1);
+        /* Listar Datos */
+        listarMisDatos(profesor1);
+        /* Registrar la asistencia */
+        registrarAsistencia(alumno2);
+        /* Eliminar asignatura */
+        quitarAsignatura(alumno1, "Idioma");
+        /* Lista general de las personas */
+        Console.WriteLine("Las personas dentro de la universidad son las siguientes: ");
         listaGeneral(listaPersonas);
-        registrarAsistencia(listaPersonas);
-        listarMisDatos(listaPersonas);
-        quitarAsignatura(listaPersonas);
-    }
 
-
-    static void listarMisDatos(List<Persona> personas)
-    {
-        foreach (Persona elemento in personas)
-        {
-            if (elemento is Alumno)
-            {
-                ((Alumno)elemento).listarMisDatos("2016579");
-            }
-            if (elemento is Profesor)
-            {
-                ((Profesor)elemento).listarMisDatos("3020151910101");
-            }
-        }
-    }
-
-    static void quitarAsignatura(List<Persona> personas)
-    {
-        foreach (Persona elemento in personas)
-        {
-            if (elemento is Alumno)
-            {
-                ((Alumno)elemento).eliminarAsignatura("Idioma");
-            }
-        }
     }
 
     static void operarRegistro(Persona elemento)
     {
         listaPersonas.Add(elemento);
+        Console.WriteLine("Se ha añadido a " + elemento);
     }
+    static void listarMisDatos(Persona elemento)
+    {
+        if (elemento is Alumno)
+        {
+            Alumno auxiliar = (Alumno)elemento;
+            auxiliar.listarMisDatos(auxiliar.Carne);
+        }
+        else if (elemento is Profesor)
+        {
+            Profesor auxiliar = (Profesor)elemento;
+            auxiliar.listarMisDatos(auxiliar.CUI);
+        }
+    }
+
+    static void quitarAsignatura(Persona elemento, string asignatura)
+    {
+        ((IOperaciones)elemento).eliminarAsignatura(asignatura);
+    }
+
     static void listaGeneral(List<Persona> personas)
     {
         Console.WriteLine($"Información General");
@@ -63,17 +65,13 @@ public class Program
             }
         }
     }
-    static void registrarAsistencia(List<Persona> personas)
+    static void registrarAsistencia(Persona elemento)
     {
-        foreach (Persona elemento in personas)
+        foreach (Persona asistencia in listaPersonas)
         {
-            if (elemento is Alumno)
+            if (asistencia.Uuid == elemento.Uuid)
             {
-                ((Alumno)elemento).TomarAsistencia();
-            }
-            else if (elemento is Profesor)
-            {
-                ((Profesor)elemento).TomarAsistencia();
+                elemento.TomarAsistencia();
             }
         }
     }
